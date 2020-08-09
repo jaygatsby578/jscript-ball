64b1d11e02bd41af384a51c52471a4b9c15f5043
Ubuntu <ubuntu@ip-172-31-39-218.us-east-2.compute.internal>
2020-08-09 05:47:27 +0000

    first

---
+++ a1.js
@@ -0,0 +1,9 @@
+function ii() {
+  var elem = document.getElementById('elem');
+  elem.addEventListener('click',function(){
+  anime({
+    targets: elem,
+    translateX: 250                
+  })              
+})
+}
